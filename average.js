ba9d425b065904eea95bcce4864b6c9cf5d311ee
rlitoncs <rj_liton@hotmail.com>
2024-04-18 17:40:42 -0400

    Added comments

--- average.js
+++ average.js
@@ -1,4 +1,12 @@
-const average = function(list) {
+/**
+ * Errors:
+ * 1) function was missing an ending brace }
+ * 2) wrong use of operator ++, the intended operator should be += to add each element
+ *
+ * Fix all the errors in both files (each file may have more than one error), without changing the implementations.
+ */
+
+function average(list) {
   let sum = 0;
   
   for (let num of list) {
@@ -6,5 +14,5 @@ const average = function(list) {
   }
   
   return sum / list.length;
-};
+}
 console.log(average([3, 5, 7]));
