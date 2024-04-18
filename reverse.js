b69791561489f5049a38e5bfff761f3360b60565
rlitoncs <rj_liton@hotmail.com>
2024-04-18 17:38:35 -0400

    Fixed all errors and linted the file

--- reverse.js
+++ reverse.js
@@ -1,8 +1,16 @@
-var input = process.argv[2];
+/**
+ * Errors:
+ * 1) original was misspelled
+ * 2) reverseList() is not an existing function, it is reverse()
+ *
+ * Fix all the errors in both files (each file may have more than one error), without changing the implementations.
+ */
+
+let input = process.argv[2];
 
 if (input)
   console.log(reverse(input));
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  return original.split('').reverse().join('');
 }
